16b8a44ed50c197c7776bdac156bee79fdca2334
2Heeae <101415696+2Heeae@users.noreply.github.com>
2022-03-21 16:28:22 +0900

    보드컨트롤러
    ㅇㅇ

--- Codelog/src/main/java/com/spring/codelog/board/controller/BoardController.java
+++ Codelog/src/main/java/com/spring/codelog/board/controller/BoardController.java
@@ -2,13 +2,17 @@ package com.spring.codelog.board.controller;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.spring.codelog.board.model.BoardVO;
 import com.spring.codelog.board.service.BoardService;
@@ -47,7 +51,37 @@ public class BoardController {
 		return "redirect:/";
 	}
 	
-
+	// 게시글 상세내용 조회, 게시글 조회수 증가 처리
+    // @RequestParam : get/post방식으로 전달된 변수 1개
+    // HttpSession 세션객체
+    @RequestMapping(value="view.do", method=RequestMethod.GET)
+    public ModelAndView view(@RequestParam int bno, HttpSession session) {
+        // 조회수 증가 처리
+        service.increaseViewcnt(bno, session);
+        // 모델(데이터)+뷰(화면)를 함께 전달하는 객체
+        ModelAndView mav = new ModelAndView();
+        // 뷰의 이름
+        mav.setViewName("board/view");
+        // 뷰에 전달할 데이터
+        mav.addObject("dto", service.read(bno));
+        return mav;
+    }
+	
+ //  게시글 수정
+    // 폼에서 입력한 내용들은 @ModelAttribute BoardVO vo로 전달됨
+    @RequestMapping(value="update.do", method=RequestMethod.POST)
+    public String update(@ModelAttribute BoardVO vo) {
+        service.update(vo);
+        return "redirect:list.do";
+    }
+    
+    //  게시글 삭제
+    @RequestMapping("delete.do")
+    public String delete(@RequestParam int bno) {
+        service.delete(bno);
+        return "redirect:list.do";
+    }
+	
 	
 	
 
