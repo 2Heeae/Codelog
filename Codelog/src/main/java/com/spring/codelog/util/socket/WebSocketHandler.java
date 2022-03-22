43c6de147bf2a4e168ad8f63e081e81b34453851
Daeun Jung <122bittersweet@gmail.com>
2022-03-22 09:33:52 +0900

    알림기능 진행중

---
+++ Codelog/src/main/java/com/spring/codelog/util/socket/WebSocketHandler.java
@@ -0,0 +1,8 @@
+package com.spring.codelog.util.socket;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+	
+
+}
