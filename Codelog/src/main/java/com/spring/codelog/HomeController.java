49389556b7ae9615b81babd22173cfeea814bfae
ychj1609 <ychj1609@gmail.com>
2022-03-30 20:23:34 +0900

    from 삭제

--- Codelog/src/main/java/com/spring/codelog/HomeController.java
+++ Codelog/src/main/java/com/spring/codelog/HomeController.java
@@ -254,16 +254,7 @@ public class HomeController {
 	   System.out.println("포스트 formT "+fromT);      
 	      model.addAttribute("recent", true);
 	      model.addAttribute("trending", false);
-	      if(fromT!=null) {
-	      if(fromT.equals("true")) {
-	    	  model.addAttribute("fromT", true);
-	    	  model.addAttribute("fromR", false);
-	      }
-	      }
-	      else {
-	    	  model.addAttribute("fromT", false);
-	    	  model.addAttribute("fromR", false);     
-	      }     
+	     
 	   
 	   HttpSession session = request.getSession();
 	   if(session.getAttribute("loginSession")!=null) {
@@ -376,17 +367,7 @@ public class HomeController {
 	  System.out.println("겟 formR "+fromR);      
       model.addAttribute("trending", true);
       model.addAttribute("recent", false);
-      if(fromR!=null) {
-      if(fromR.equals("true")) {
-    	  model.addAttribute("fromR", true);
-          model.addAttribute("fromT", false);
-
-      }
-      }
-      else {
-    	  model.addAttribute("fromR", false);
-          model.addAttribute("fromT", false);    
-      }
+    
       return "home";
    }
    
@@ -452,16 +433,7 @@ public class HomeController {
 	  System.out.println("포스트 formR "+fromR);
       model.addAttribute("trending", true);
       model.addAttribute("recent", false);
-      if(fromR!=null) {
-      if(fromR.equals("true")) {
-    	  model.addAttribute("fromR", true);
-          model.addAttribute("fromT", false);
-      }
-      }
-      else {
-    	  model.addAttribute("fromR", false);
-          model.addAttribute("fromT", false);
-      }
+     
       
 	   System.out.println("-----------------------------------------------------------------------");
 	   
